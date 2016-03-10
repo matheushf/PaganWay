3520898096b645906035e02d6745a9e77b92fb56
matheushf <matheushf@hf-kali>
2016-03-09 23:30:48 -0300

    adjustments

--- head.php
+++ head.php
@@ -44,7 +44,7 @@
                                 Celtics<span class="caret"></span>
                             </a>
                             <ul class="dropdown-menu" role="menu">                                
-                                <li><a href="/pages/celticHistory.php">History</a></li>
+                                <li><a href="/modules/celticHistory.php">History</a></li>
                                 <li><a href="example.php">Some Facts</a></li>
                             </ul>
                         </li>
